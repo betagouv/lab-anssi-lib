96171a9083383da985a5ee5b5a3784e59add08d1
Alexis B <abrones1@gmail.com>
2025-04-30 15:10:00 +0200

    Décompose le html en section
    ... Lors du rendu du markdown venant de Crisp, on ajoute dynamiquement des sections autour des blocs logiques (titre + contenu)

--- src/cms/crispMarkdown.ts
+++ src/cms/crispMarkdown.ts
@@ -1,4 +1,4 @@
-import { Lexer, marked, Parser, RendererObject, Tokens } from 'marked';
+import { Lexer, Marked, Parser, RendererObject, Tokens } from 'marked';
 
 const extensionBoite = (regex: RegExp, nom: string, classe: string) => ({
   name: nom,
@@ -31,6 +31,7 @@ class CrispMarkdown {
   private contenuHTML: string | null = null;
   private aDejaParse: boolean = false;
   private tdm: any[] = [];
+  private marked: Marked;
 
   constructor(private contenuMarkdown: string) {
     const boiteAide = extensionBoite(/^\|([^|\n]+)/, 'boiteAide', 'aide');
@@ -70,6 +71,40 @@ class CrispMarkdown {
       },
     };
 
+    // Source d'inspiration pour la gestion des sections :
+    // https://github.com/markedjs/marked/discussions/2889
+    let niveauDeSection = 0;
+    const sectionRegexp = new RegExp(`^(# )[^]*?(?:\\n(?=\\1)|$)`);
+
+    const section = {
+      name: 'sectionBlock',
+      level: 'block',
+      start(source: string) {
+        return source.match(/^#/m)?.index;
+      },
+      tokenizer(this: { lexer: Lexer }, source: string) {
+        if (niveauDeSection > 0) return;
+        const match = source.match(sectionRegexp);
+        if (!match) {
+          return;
+        }
+
+        niveauDeSection++;
+        const tokens = this.lexer.blockTokens(match[0]);
+        niveauDeSection--;
+
+        return {
+          type: 'sectionBlock',
+          raw: match[0],
+          level: 1,
+          tokens,
+        };
+      },
+      renderer(this: { parser: Parser }, token: Tokens.Generic) {
+        return `<section>${this.parser.parse(token.tokens!)}</section>`;
+      },
+    };
+
     const moteurDeRendu = (that: CrispMarkdown): RendererObject => ({
       heading(this: RendererObject, ...[texte, profondeur]: any[]) {
         const slugDuTitre = texte.toLowerCase().replace(/\W+/g, '-');
@@ -89,14 +124,14 @@ class CrispMarkdown {
       },
     });
 
-    marked.use({
+    this.marked = new Marked({
       renderer: moteurDeRendu(this),
-      extensions: [boiteAide, boiteInfo, boiteAlerte, video],
-    });
+      extensions: [boiteAide, boiteInfo, boiteAlerte, video, section],
+    })
   }
 
   parseLeMarkdown() {
-    this.contenuHTML = marked.parse(this.contenuMarkdown) as string;
+    this.contenuHTML = this.marked.parse(this.contenuMarkdown) as string;
     this.aDejaParse = true;
   }
 
