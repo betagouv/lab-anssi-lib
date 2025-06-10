86b455ba99a2dcc5e37984c1384e1ceecbc18a83
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-06-10 17:33:36 +0200

    Ajoute les méthodes permettant de récupérer les articles d'une catégorie, et les sections de cette catégorie

--- src/cms/cmsCrisp.ts
+++ src/cms/cmsCrisp.ts
@@ -1,5 +1,10 @@
-import { AdaptateurCmsCrisp } from './adaptateurCmsCrisp';
+import {
+  AdaptateurCmsCrisp,
+  ResumeArticleCrisp,
+  SectionCrisp,
+} from './adaptateurCmsCrisp';
 import CrispMarkdown from './crispMarkdown';
+import { ErreurArticleCrispIntrouvable } from '../erreurs';
 
 export type PageHtmlCrisp = {
   titre: string;
@@ -7,6 +12,18 @@ export type PageHtmlCrisp = {
   description: string;
   tableDesMatieres: any[];
 };
+
+type ArticleMarkdownCrispAvecSection = {
+  titre: string;
+  contenuMarkdown: string;
+  description: string;
+  section: {
+    id?: string;
+    nom?: string;
+  };
+};
+
+type ResumeArticleCrispAvecSlug = ResumeArticleCrisp & { slug: string | null };
 
 export class CmsCrisp {
   adaptateurCmsCrisp: AdaptateurCmsCrisp;
@@ -28,5 +45,50 @@ export class CmsCrisp {
       description,
       tableDesMatieres: crispMarkdown.tableDesMatieres(),
     };
+  }
+
+  async recupereArticleCategorie(
+    slug: string,
+    idCategorie: string
+  ): Promise<ArticleMarkdownCrispAvecSection> {
+    const articles =
+      await this.adaptateurCmsCrisp.recupereArticlesCategorie(idCategorie);
+    const article = articles.find((a) => this.extraitSlugArticle(a) === slug);
+    if (!article) {
+      throw new ErreurArticleCrispIntrouvable();
+    }
+    const articleCrisp = await this.adaptateurCmsCrisp.recupereArticle(
+      article.id
+    );
+    return {
+      ...articleCrisp,
+      section: { id: article.section.id, nom: article.section.nom },
+    };
+  }
+
+  async recupereSectionsCategorie(
+    idCategorie: string
+  ): Promise<SectionCrisp[]> {
+    return this.adaptateurCmsCrisp.recupereSectionsCategorie(idCategorie);
+  }
+
+  async recupereArticlesCategorie(
+    idCategorie: string
+  ): Promise<ResumeArticleCrispAvecSlug[]> {
+    const articles =
+      await this.adaptateurCmsCrisp.recupereArticlesCategorie(idCategorie);
+    return articles.map((article) => {
+      const slug = this.extraitSlugArticle(article);
+      return { ...article, slug: slug || null };
+    });
+  }
+
+  private extraitSlugArticle(article: ResumeArticleCrisp) {
+    try {
+      const regex = /\/article\/(.*)-[a-zA-Z0-9]{1,10}\//gm;
+      return regex.exec(article.url)?.[1];
+    } catch (e) {
+      return null;
+    }
   }
 }
