66815b08c1b71e9d5d19e18f932f8881b4cc90e6
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-06-11 11:09:42 +0200

    Exporte l'erreur d'article crisp introuvable

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { CmsCrisp, type PageHtmlCrisp } from './cms/cmsCrisp';
 export { AdaptateurProfilAnssi } from './profilAnssi/adaptateurProfilAnssi';
+
+export { ErreurArticleCrispIntrouvable } from './erreurs';
